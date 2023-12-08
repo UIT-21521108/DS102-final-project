d6201e6ed4173fe9ae2b53c1710878e3fdecde88
minlee <118150826+UIT-21521108@users.noreply.github.com>
2023-12-08 10:12:28 +0700

    up

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import pickle as pkl
 import numpy as np
 
 class_list = {'0': 'Negative', '1': 'Positive', '2': 'Neutral'}
-input_ec = open('ec_vsfc.pkl', 'rb')
+
+input_ec = open('UIT-21521108/VSFC-text/ec_vsfc.pkl', 'rb')
 encoder = pkl.load(input_ec)
 
-input_md = open('lrc_vsfc.pkl', 'rb')
+input_md = open('UIT-21521108/VSFC-text/lrc_vsfc.pkl', 'rb')
 model = pkl.load(input_md)
 
 st.title('Sentiment Analysis from Vietnamese Students Feedback')
