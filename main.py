1330dcb471623c95d6bfbd92ae16637b705d56f1
minlee <118150826+UIT-21521108@users.noreply.github.com>
2023-12-29 03:01:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pickle as pkl
 import numpy as np
 import sklearn
+from PIL import Image
+import requests
+from io import BytesIO
 
 class_list = {'negative': 'Negative', 'positive': 'Positive', 'neutral': 'Neutral'}
 input_ec = open('Tfd.pkl', 'rb')
@@ -11,7 +14,10 @@ input_md = open('svc.pkl', 'rb')
 model = pkl.load(input_md)
 
 st.title('Sentiment Analysis from Vietnamese Students\' Feedback')
-
+image_path_or_url = 'https://strategyeducation.co.uk/wp-content/uploads/2020/05/The-Power-of-Feedback-Which-When-and-How.jpg'
+response = requests.get(image_path_or_url)
+image = Image.open(BytesIO(response.content))
+st.image(image, caption='Your Image Caption', use_column_width=True)
 st.header('Write a feedback')
 text = st.text_area('', '')
 
